915b4109dabc118b70d5def36873d2b1ab330b1d
Raghu K <raghuk.vit@gmail.com>
2023-01-11 09:00:24 +0530

    Update tf-modules/tf-module-mutable-app/

--- route53.tf
+++ route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "component" {
   count   = var.component == "frontend" ? 0 : 1
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "${var.env}-${var.component}.${var.domain}"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 30
   records = [local.dns_name]
 }
