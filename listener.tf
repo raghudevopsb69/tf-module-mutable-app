b45e70d4e83f7b99862b6a968e0964ec56ba23dc
Raghu K <raghuk.vit@gmail.com>
2023-01-11 08:40:15 +0530

    Update tf-modules/tf-module-mutable-app/

--- listener.tf
+++ listener.tf
@@ -1,14 +1,14 @@
-//resource "aws_lb_listener" "listener_public" {
-//  count             = var.component == "frontend" ? 1 : 0
-//  load_balancer_arn = local.arn
-//  port              = "80"
-//  protocol          = "HTTP"
-//
-//  default_action {
-//    type             = "forward"
-//    target_group_arn = aws_lb_target_group.tg.arn
-//  }
-//}
+resource "aws_lb_listener" "listener_public" {
+  count             = var.component == "frontend" ? 1 : 0
+  load_balancer_arn = local.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
 
 resource "aws_lb_listener" "listener_private" {
   count             = var.component == "frontend" ? 0 : 1
