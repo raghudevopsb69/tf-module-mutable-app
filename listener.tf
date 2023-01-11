476f3672c254b014aa73499c3ff2046a87229178
Raghu K <raghuk.vit@gmail.com>
2023-01-11 07:55:15 +0530

    Update tf-modules/tf-module-mutable-app/

--- listener.tf
+++ listener.tf
@@ -1,10 +1,10 @@
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = local.lb_arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
+//resource "aws_lb_listener" "listener" {
+//  load_balancer_arn = local.lb_arn
+//  port              = "80"
+//  protocol          = "HTTP"
+//
+//  default_action {
+//    type             = "forward"
+//    target_group_arn = aws_lb_target_group.tg.arn
+//  }
+//}
