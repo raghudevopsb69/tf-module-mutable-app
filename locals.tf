82d0e9d4dd98223cfdc5f37c660dac0e858188e9
Raghu K <raghuk.vit@gmail.com>
2023-01-11 08:10:46 +0530

    Update tf-modules/tf-module-mutable-app/

--- locals.tf
+++ locals.tf
@@ -7,8 +7,8 @@ locals {
 
   allow_app_access = var.component == "frontend" ? local.public_subnets_cidr : local.app_subnets_cidr
 
-  public_lb_arn  = lookup(lookup(var.load_balancers, "public", null), "arn", null)
-  private_lb_arn = lookup(lookup(var.load_balancers, "private", null), "arn", null)
+  public_lb_arn  = lookup(lookup(var.load_balancers, "public", null), "lb_arn", null)
+  private_lb_arn = lookup(lookup(var.load_balancers, "private", null), "lb_arn", null)
 
   lb_arn = var.component == "frontend" ? local.public_lb_arn : local.private_lb_arn
 
