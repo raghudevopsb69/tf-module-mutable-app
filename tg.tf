63c2fcd80231f61131825fb4c108916718098806
Raghu K <raghuk.vit@gmail.com>
2023-01-12 08:25:55 +0530

    Update tf-modules/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -1,6 +1,14 @@
 resource "aws_lb_target_group" "tg" {
   name     = "${var.component}-${var.env}"
-  port     = 80
+  port     = var.app_port
   protocol = "HTTP"
   vpc_id   = local.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
+    path                = "/health"
+  }
 }
