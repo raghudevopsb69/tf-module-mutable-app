3ce34d1f4eb67945930ca0a19370495fb0212292
Raghu K <raghuk.vit@gmail.com>
2023-01-18 07:48:17 +0530

    Update tf-modules/tf-module-mutable-app/

--- asg.tf
+++ asg.tf
@@ -39,5 +39,11 @@ resource "aws_autoscaling_group" "asg" {
     propagate_at_launch = true
   }
 
+  tag {
+    key                 = "Monitor"
+    value               = "yes"
+    propagate_at_launch = true
+  }
+
 }
 
